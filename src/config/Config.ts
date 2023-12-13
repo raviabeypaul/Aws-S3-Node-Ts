6ad6cc090481f351a98d1567531c619913f48bb1
ravi <raviabeypaul@gmail.com>
2023-12-13 13:47:16 -0500

    updated config and .env

--- src/config/Config.ts
+++ src/config/Config.ts
@@ -1,7 +1,7 @@
 export const CONFIG = {
-    AWS_ACCESS_KEY : ()=> process.env.AWS_ACCESS_KEY,
-    AWS_SECRET_ACCESS_ID : ()=> process.env.AWS_SECRET_ACCESS_ID,
-    AWS_REGION: ()=> process.env.AWS_REGION,
+    AWS_ACCESS_KEY : ()=> process.env.ACCESS_KEY,
+    AWS_SECRET_ACCESS_ID : ()=> process.env.SECRET_ACCESS_ID,
+    AWS_REGION: ()=> process.env.REGION,
     BUCKET_NAME :()=> process.env.BUCKET_NAME,
     BUCKET_OBJECT_KEY : ()=> process.env.BUCKET_OBJECT_KEY
 };
