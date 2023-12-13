265e8ae0ab2722500fc3f0deee540892153623be
ravi <raviabeypaul@gmail.com>
2023-12-13 18:06:06 -0500

    comit undo certificates

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,13 @@ try {
   // validateConfig([], ["PORT"]);
   info("Config is valid, starting Express server");
 
-  const app = createServer()
-  const sslServer  = https.createServer({
-    key : fs.readFileSync(path.join(__dirname, ".." ,'cert','key.pem')),
-    cert : fs.readFileSync(path.join(__dirname,"..",'cert','cert.pem'))
-  },app).listen(8080);
+  const app = createServer().listen(8080)
+  // const sslServer  = https.createServer({
+  //   key : fs.readFileSync(path.join(__dirname, ".." ,'cert','key.pem')),
+  //   cert : fs.readFileSync(path.join(__dirname,"..",'cert','cert.pem'))
+  // },app).listen(8080);
   
-  info(`Listening on port : ${sslServer.address()["port"]}`);
+  info(`Listening on port : ${app.address()["port"]}`);
 } catch (e) {
   error("", e);
   process.exit(1);
