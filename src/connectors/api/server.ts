29da73488470c0e7123cb88a511e876cdf71e032
ravi <raviabeypaul@gmail.com>
2023-12-13 14:37:22 -0500

    updated cors options

--- src/connectors/api/server.ts
+++ src/connectors/api/server.ts
@@ -6,12 +6,21 @@ import cors from 'cors';
 
 export const createServer = (): Application => {
   const app: Application = express();
-  const options = {
-    origin: ['https://raviabeypaul.github.io/','https://raviabeypaul.github.io/React-CRUD-Seacrh-Sample/', 'http://localhost:3000/'],
-    }
+  const corsOpts = {
+  origin: '*',
+
+  methods: [
+    'GET',
+    'POST',
+  ],
+
+  allowedHeaders: [
+    'Content-Type',
+  ],
+};
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(cors(options));
+  app.use(cors(corsOpts));
   routes(app);
   app.use(ExpressErrorMiddleware)
   return app;
